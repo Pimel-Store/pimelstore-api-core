92b90b968ae117d49b4fe0541318a612f0cd11f7
Fábio Abreu <fabio.abreu@robozz.com.br>
2025-11-25 23:54:25 -0300

    first step for vercel updates

--- api/home.ts
+++ api/home.ts
@@ -1,7 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // Simula chamada assíncrona
   const data = await Promise.resolve({ message: 'API funcionando!' });
   
   res.status(200).json(data);
--- src/dev.ts
+++
@@ -1,4 +0,0 @@
-import app from './index';
-
-const PORT = 3000;
-app.listen(PORT, () => console.log(`Servidor local rodando em http://localhost:${PORT}`));
