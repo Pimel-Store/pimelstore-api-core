0400a4159f86d9e1204c892bad976dd888512ce9
Fábio Abreu <fabio.abreu@robozz.com.br>
2025-11-25 23:55:36 -0300

    change home message

--- api/home.ts
+++ api/home.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const data = await Promise.resolve({ message: 'API funcionando!' });
+  const data = await Promise.resolve({ message: 'THis is the API for PimelStore!' });
   
   res.status(200).json(data);
 }
