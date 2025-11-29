4d9e39f2154db6652e158d574e5accde16020989
Fábio Abreu <fabio.abreu@robozz.com.br>
2025-11-29 02:54:57 -0300

    options rules

--- api/auth/login.ts
+++ api/auth/login.ts
@@ -10,6 +10,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const { email, password } = req.body as User;
 
   try {
