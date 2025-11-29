9347129ece7e9f789f06e04460d38abdf2087580
Fábio Abreu <fabio.abreu@robozz.com.br>
2025-11-29 03:48:55 -0300

    update get token

--- api/auth/token.ts
+++ api/auth/token.ts
@@ -6,6 +6,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   
   try {
     const securutyValidation = await securityRules(req);
